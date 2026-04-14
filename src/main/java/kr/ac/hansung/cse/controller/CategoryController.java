25f0a73d6d267a0a52add5e4700ec8444acf235e
seojunheo <hry0112@gmail.com>
2026-04-14 13:51:04 +0900

    feat: CategoryController 구현 (GET/POST)

--- src/main/java/kr/ac/hansung/cse/controller/CategoryController.java
+++ src/main/java/kr/ac/hansung/cse/controller/CategoryController.java
@@ -1,4 +1,62 @@
 package kr.ac.hansung.cse.controller;
 
+import jakarta.validation.Valid;
+import kr.ac.hansung.cse.exception.DuplicateCategoryException;
+import kr.ac.hansung.cse.model.Category;
+import kr.ac.hansung.cse.model.CategoryForm;
+import kr.ac.hansung.cse.service.CategoryService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/categories")
 public class CategoryController {
+    private  final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    // GET /categories => 목록
+    @GetMapping
+    public String listCategories(Model model) {
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "categoryList";
+    }
+
+    // GET /categories/create => 등록 폼
+    @GetMapping("/create")
+    public String showCategoryForm(Model model) {
+        model.addAttribute("categoryForm", new CategoryForm());
+        return "categoryForm";
+    }
+
+    // Post /categories/create => 등록 처리
+    @PostMapping("/create")
+    public String createCategory(@Valid @ModelAttribute CategoryForm categoryForm, BindingResult bindingResult,
+                                 RedirectAttributes redirectAttributes){
+        if (bindingResult.hasErrors()) return "categoryForm";
+        try{
+            categoryService.createCategory(categoryForm.getName());
+            redirectAttributes.addFlashAttribute("successMessage", "등록 완료");
+        } catch (DuplicateCategoryException e) {
+            bindingResult.rejectValue("name", "duplicate", e.getMessage());
+            return "redirect:/categories";
+        }
+        return "redirect:/categories";
+    }
+
+    // Post /categories/{id}/delete => 삭제 처리
+    public String deleteCategory(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        try {
+            categoryService.deleteCategory(id);
+            redirectAttributes.addFlashAttribute("successMessage","삭제 완료");
+        } catch (IllegalStateException e) {
+            redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+        }
+        return "redirect:/categories";
+    }
 }
